4bcdda30dd800998ca4558edb50c4919365b40be
anniechakraborty <the.demented.genius29@gmail.com>
2024-12-25 22:31:58 +0100

    parsed the html and wrote into a file

--- src/app.py
+++ src/app.py
@@ -1,17 +1,28 @@
 import requests
+import webbrowser
+import os
 from bs4 import BeautifulSoup
 
-# Step 1: Send an HTTP request to the website
-url = "https://medium.com/spring-boot/building-a-reusable-spring-boot-kafka-messaging-system-with-avro-serialization-3c7bc430e888"
+url = input('Please enter the url to parse : ').strip()
 response = requests.get(url)
+try:    
+    if response.status_code == 200:
+        print("Successfully accessed the website!")
+    else:
+        print(f"Failed to access the website. Status code: {response.status_code}")
+        exit()
 
-# Step 2: Check the HTTP response status
-if response.status_code == 200:
-    print("Successfully accessed the website!")
-else:
-    print(f"Failed to access the website. Status code: {response.status_code}")
-    exit()
+    html_content = response.text
+    soup = BeautifulSoup(html_content, 'html.parser')
 
-# Step 3: Parse the HTML content
-html_content = response.text
-soup = BeautifulSoup(html_content, 'html.parser')
+    # Writing the soup content into an html file
+    file_name = 'output/pageContent.html'
+    with open(file_name, "w", encoding="utf-8") as file:
+        file.write(soup.prettify())
+
+    # Opening the generated HTML file in a web browser
+    file_path = os.path.abspath(file_name)
+    webbrowser.open(f"file://{file_path}")
+
+except Exception as e:
+    print(f"An error occurred: {str(e)}")
